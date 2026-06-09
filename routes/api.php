478ded13bd9895c6235b5712b3df871c1600e612
mamebassine <mamebassine06@gmail.com>
2026-06-09 11:52:24 +0000

    Correction des migrations tours et cotisations

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,32 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TontineController;
+use App\Http\Controllers\Api\TourController;
+use App\Http\Controllers\Api\CotisationController;
+use App\Http\Controllers\Api\NotificationController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/profile', [AuthController::class,'profile']);
+
+    Route::post('/logout', [AuthController::class,'logout']);
+
+});
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::apiResource('tontines', TontineController::class);
+
+    Route::apiResource('tours', TourController::class);
+
+    Route::apiResource('cotisations', CotisationController::class);
+
+    Route::apiResource('notifications', NotificationController::class);
+
+});
